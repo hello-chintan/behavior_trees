d8fb443f560892ecd3e2770b0c5233ad53ca38c5
Chintan Desai <cdesai@hello-robot.com>
2023-11-16 10:48:00 -0800

    Initial solution

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-# Sample BehaviorTree.CPP project
+# This is a project to experiment with task scheduling using the behavior tree library BehaviorTree.CPP
 
 This project is compatible with version 4.X of [BehaviorTree.CPP](https://github.com/BehaviorTree/BehaviorTree.CPP).
 
-The **CMakeLists.txt** and **package.xml** files are compatible with both ROS and ROS2.
+# Setup Instructions
+
+## Install ROS 2 Humble
+This project has a dependency on ROS 2 as it uses the colcon build system. Follow instructions for [installing ROS 2 Humble](https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html).
+
+## Install BehaviorTree.CPP library
+Begin by [creating a ROS 2 workspace](https://docs.ros.org/en/humble/Tutorials/Beginner-Client-Libraries/Creating-A-Workspace/Creating-A-Workspace.html):
+```bash
+mkdir -p ~/ament_ws/src
+cd ~/ament_ws/src
+```
+
+clone the BehaviorTree.CPP library along with this package using the following commands:
+```bash
+git clone https://github.com/BehaviorTree/BehaviorTree.CPP.git
+git clone ...
+```
+
+Build the workspace using colcon to ensure smooth progress:
+```bash
+cd ~/ament_ws
+colcon build
+```
+
+Next, install Groot2, the BT GUI tool, to visualize, edit and log behavior trees. Follow instructions on the [Groot2 installation page](https://www.behaviortree.dev/groot/).
+
+# Running the code
+You are now ready to run the code. Assuming everything is setup correctly, execute the code in example_one.cpp as follows:
+```bash
+ros2 run behavior_trees btcpp_sample
+```
---
+++ example_one.cpp
@@ -0,0 +1,370 @@
+#include "behaviortree_cpp/bt_factory.h"
+#include "behaviortree_cpp/xml_parsing.h"
+#include "behaviortree_cpp/loggers/groot2_publisher.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+
+using namespace BT;
+
+// Example of custom SyncActionNode (synchronous action)
+// without ports.
+class TaskStart : public BT::SyncActionNode
+{
+public:
+    TaskStart(const std::string& name) :
+        BT::SyncActionNode(name, {})
+    {}
+
+    // You must override the virtual function tick()
+    BT::NodeStatus tick() override
+    {
+        std::cout << "Starting execution of tasks!" << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class TaskEnd : public BT::SyncActionNode
+{
+public:
+    TaskEnd(const std::string& name) :
+        BT::SyncActionNode(name, {})
+    {}
+
+    // You must override the virtual function tick()
+    BT::NodeStatus tick() override
+    {
+        std::cout << "Task execution ended successfully!" << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class TaskA : public BT::StatefulActionNode
+{
+public:
+    TaskA(const std::string& name, const BT::NodeConfig& config) :
+        BT::StatefulActionNode(name, config)
+    {}
+
+    static BT::PortsList providedPorts()
+    {
+        // amount of milliseconds that we want to sleep
+        return{ BT::InputPort<int>("msec") };
+    }
+
+    NodeStatus onStart() override
+    {
+      int msec = 0;
+      getInput("msec", msec);
+
+      std::cout << "Executing task A" << std::endl;
+      
+      if( msec <= 0 ) {
+        // No need to go into the RUNNING state
+        std::cout << "Finished task A" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        // once the deadline is reached, we will return SUCCESS.
+        deadline_ = std::chrono::system_clock::now() + std::chrono::milliseconds(msec);
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    /// method invoked by an action in the RUNNING state.
+    NodeStatus onRunning() override
+    {
+      if ( std::chrono::system_clock::now() >= deadline_ ) {
+        std::cout << "Finished task A" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    void onHalted() override
+    {
+      // nothing to do here...
+      std::cout << "Task A interrupted" << std::endl;
+    }
+
+private:
+    std::chrono::system_clock::time_point deadline_;
+};
+
+class TaskB : public BT::StatefulActionNode
+{
+public:
+    TaskB(const std::string& name, const BT::NodeConfig& config) :
+        BT::StatefulActionNode(name, config)
+    {}
+
+    static BT::PortsList providedPorts()
+    {
+        // amount of milliseconds that we want to sleep
+        return{ BT::InputPort<int>("msec") };
+    }
+
+    NodeStatus onStart() override
+    {
+      int msec = 0;
+      getInput("msec", msec);
+
+      std::cout << "Executing task B" << std::endl;
+      
+      if( msec <= 0 ) {
+        // No need to go into the RUNNING state
+        std::cout << "Finished task B" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        // once the deadline is reached, we will return SUCCESS.
+        deadline_ = std::chrono::system_clock::now() + std::chrono::milliseconds(msec);
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    /// method invoked by an action in the RUNNING state.
+    NodeStatus onRunning() override
+    {
+      if ( std::chrono::system_clock::now() >= deadline_ ) {
+        std::cout << "Finished task B" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    void onHalted() override
+    {
+      // nothing to do here...
+      std::cout << "Task B interrupted" << std::endl;
+    }
+
+private:
+    std::chrono::system_clock::time_point deadline_;
+};
+
+class TaskC : public BT::StatefulActionNode
+{
+public:
+    TaskC(const std::string& name, const BT::NodeConfig& config) :
+        BT::StatefulActionNode(name, config)
+    {}
+
+    static BT::PortsList providedPorts()
+    {
+        // amount of milliseconds that we want to sleep
+        return{ BT::InputPort<int>("msec") };
+    }
+
+    NodeStatus onStart() override
+    {
+      int msec = 0;
+      getInput("msec", msec);
+
+      std::cout << "Executing task C" << std::endl;
+      
+      if( msec <= 0 ) {
+        // No need to go into the RUNNING state
+        std::cout << "Finished task C" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        // once the deadline is reached, we will return SUCCESS.
+        deadline_ = std::chrono::system_clock::now() + std::chrono::milliseconds(msec);
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    /// method invoked by an action in the RUNNING state.
+    NodeStatus onRunning() override
+    {
+      if ( std::chrono::system_clock::now() >= deadline_ ) {
+        std::cout << "Finished task C" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    void onHalted() override
+    {
+      // nothing to do here...
+      std::cout << "Task C interrupted" << std::endl;
+    }
+
+private:
+    std::chrono::system_clock::time_point deadline_;
+};
+
+class TaskD : public BT::StatefulActionNode
+{
+public:
+    TaskD(const std::string& name, const BT::NodeConfig& config) :
+        BT::StatefulActionNode(name, config)
+    {}
+
+    static BT::PortsList providedPorts()
+    {
+        // amount of milliseconds that we want to sleep
+        return{ BT::InputPort<int>("msec") };
+    }
+
+    NodeStatus onStart() override
+    {
+      int msec = 0;
+      getInput("msec", msec);
+
+      std::cout << "Executing task D" << std::endl;
+      
+      if( msec <= 0 ) {
+        // No need to go into the RUNNING state
+        std::cout << "Finished task D" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        // once the deadline is reached, we will return SUCCESS.
+        deadline_ = std::chrono::system_clock::now() + std::chrono::milliseconds(msec);
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    /// method invoked by an action in the RUNNING state.
+    NodeStatus onRunning() override
+    {
+      if ( std::chrono::system_clock::now() >= deadline_ ) {
+        std::cout << "Finished task D" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    void onHalted() override
+    {
+      // nothing to do here...
+      std::cout << "Task D interrupted" << std::endl;
+    }
+
+private:
+    std::chrono::system_clock::time_point deadline_;
+};
+
+class TaskE : public BT::StatefulActionNode
+{
+public:
+    TaskE(const std::string& name, const BT::NodeConfig& config) :
+        BT::StatefulActionNode(name, config)
+    {}
+
+    static BT::PortsList providedPorts()
+    {
+        // amount of milliseconds that we want to sleep
+        return{ BT::InputPort<int>("msec") };
+    }
+
+    NodeStatus onStart() override
+    {
+      int msec = 0;
+      getInput("msec", msec);
+
+      std::cout << "Executing task E" << std::endl;
+      
+      if( msec <= 0 ) {
+        // No need to go into the RUNNING state
+        std::cout << "Finished task E" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        // once the deadline is reached, we will return SUCCESS.
+        deadline_ = std::chrono::system_clock::now() + std::chrono::milliseconds(msec);
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    /// method invoked by an action in the RUNNING state.
+    NodeStatus onRunning() override
+    {
+      if ( std::chrono::system_clock::now() >= deadline_ ) {
+        std::cout << "Finished task E" << std::endl;
+        return NodeStatus::SUCCESS;
+      }
+      else {
+        return NodeStatus::RUNNING;
+      }
+    }
+
+    void onHalted() override
+    {
+      // nothing to do here...
+      std::cout << "Task E interrupted" << std::endl;
+    }
+
+private:
+    std::chrono::system_clock::time_point deadline_;
+};
+
+void createFile(BT::BehaviorTreeFactory & factory){
+    std::string xml_models = BT::writeTreeNodesModelXML(factory);
+    
+    // File name
+    const std::string file_name = "./install/btcpp_sample/lib/btcpp_sample/bt_xml/tree_models.xml";
+
+    // Open a file for writing
+    std::ofstream file(file_name);
+
+    // Check if the file is open
+    if (file.is_open()) {
+        // Write the string to the file
+        file << xml_models;
+
+        // Close the file
+        file.close();
+
+        std::cout << "String successfully saved to " << file_name << std::endl;
+    } else {
+        std::cerr << "Error opening the file " << file_name << std::endl;
+    }
+}
+
+int main()
+{
+    // We use the BehaviorTreeFactory to register our custom nodes
+    BT::BehaviorTreeFactory factory;
+
+    // The recommended way to create a Node is through inheritance.
+    factory.registerNodeType<TaskStart>("TaskStart");
+    factory.registerNodeType<TaskA>("TaskA");
+    factory.registerNodeType<TaskB>("TaskB");
+    factory.registerNodeType<TaskC>("TaskC");
+    factory.registerNodeType<TaskD>("TaskD");
+    factory.registerNodeType<TaskE>("TaskE");
+    factory.registerNodeType<TaskEnd>("TaskEnd");
+
+    // createFile(factory);
+
+    // Trees are created at deployment-time (i.e. at run-time, but only 
+    // once at the beginning). 
+        
+    // IMPORTANT: when the object "tree" goes out of scope, all the 
+    // TreeNodes are destroyed
+    auto tree = factory.createTreeFromFile("./install/btcpp_sample/lib/btcpp_sample/bt_xml/my_tree.xml");
+
+    // Connect with Groot2
+    BT::Groot2Publisher publisher(tree);
+
+    // To "execute" a Tree you need to "tick" it.
+    // The tick is propagated to the children based on the logic of the tree.
+    // In this case, the entire sequence is executed, because all the children
+    // of the Sequence return SUCCESS.
+    tree.tickWhileRunning();
+
+    return 0;
+}
